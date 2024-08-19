773043315d2cb64ef39d00073df52793c44c733b
UsmanKhan321123 <uk732161@gmail.com>
2024-08-19 12:58:16 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+let username;
+let myName = "Muhammad Usman Khan";
+let message = "Hello World";
+alert(message);
+let stdName= "Jhone Doe";
+let stdAge= 15;
+let stdCourse = "Certifie Mobile Application Development";
+alert(stdName)
+alert(stdAge)
+alert(stdCourse)
+
+let pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(pizza);
+
+let email;
+email = "uk732161@gmail.com"
+alert(email)
+
+
+
+let strToBeConcatenate ="I am trying to learn from the Book " 
+let book;
+book = "A smarter way to learn Javascript"
+alert( strToBeConcatenate+book)
+
+let age = 22 ;
+alert("I am " + age + " years old");
+
+let n = 14 ;
+alert("You have visited this site " + n + " times" )
+
+
+let birthYear ;
+birthYear  = 2002;
+document.write("My birth Year is " + birthYear + "\n  ")
+document.write("\nDatatype of my declared variable is " + typeof(birthYear))
+
+
+let storeName = "xyzClothing.com";
+let Visitor = "abc";
+let product = "T-shirt";
+let quantity = 5;
+
+document.write(Visitor + " " + "ordered " + quantity + " "+ product + "s" + " " +  "on " + storeName )  
+
+
